c6708f5c08b4d0cd3ea2436046efd286d41b7671
Abdullah Mian <shepardabdullah@hotmail.com>
2022-08-09 00:52:55 -0500

    add logic for color picker + size slider

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let color = "black";
 let click = true;
 
+const colorPicker = document.querySelector(".color-picker");
+const sliderLabel = document.querySelector(".slider-label");
+let slider = document.querySelector(".slider");
 
 
+// creates a grid of empty divs arranging them into our canvas/drawing surface
 function populateCanvas(size) {
-    let canvas = document.querySelector(".canvas ")
+    let canvas = document.querySelector(".canvas")
     let squares = canvas.querySelectorAll("div")
     squares.forEach(div => div.remove)
     canvas.style.gridTemplateColumns = `repeat(${size} , 1fr)`
@@ -20,16 +24,28 @@ function populateCanvas(size) {
     }
 }
 
-populateCanvas(16);
+// calls populateCanvas function to create a default 50x50 canvas upon page load
+populateCanvas(50);
 
+
+// function that changes size of board by calling populateCanvas function
+// clears the drawing space when canvas size is changed
 function changeSize(input) {
     if (input >= 1 && input <= 100) {
     populateCanvas(input)
-    } else {
-        console.log("invalid size parameter")
+    clearCanvas();
     }
 }
 
+
+// renders canvas size changes to user in text
+function updateSliderLabel() {
+    sliderLabel.innerText = `${slider.value} x ${slider.value}`;
+}
+
+
+// function that "draws" by changing background color of any div it rolls
+// while "click" is true
 function colorSquare() {
     if (click) {
         if (color === "random") {
@@ -41,11 +57,19 @@ function colorSquare() {
 }
 
 
+// handles button color selections
 function changeColor(choice) {
     color = choice;
 }
 
 
+// handles color picker tool selections
+function selectColor(event) {
+    color = event.target.value;
+    colorSquare();
+}
+
+// function to reset all divs in canvas to white essentially "clearing" the canvas
 function clearCanvas() {
     let canvas = document.querySelector(".canvas")
     let squares = canvas.querySelectorAll("div")
@@ -53,6 +77,22 @@ function clearCanvas() {
 }
 
 
-document.querySelector("body").addEventListener("click", () => {
-    click = !click;
+// renders canvas size changes to user in text
+function updateSliderLabel() {
+    sliderLabel.innerText = `${slider.value} x ${slider.value}`;
+}
+
+
+// adds functionality to canvas size slider
+slider.addEventListener("change", (e) => changeSize(e.target.value));
+slider.addEventListener("mousemove", (e) => updateSliderLabel(e.target.value));
+// adds functionality to colorPicker
+colorPicker.addEventListener("change", (e) => selectColor(e));
+
+
+// 
+document.querySelector("body").addEventListener("click", (e) => {
+    if(e.target.tagName != "BUTTON") {
+        click = !click
+    }
 });
